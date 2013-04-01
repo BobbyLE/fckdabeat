0c3340d77cad3cc0a8cc8bf55289f75db5c33bba
Bobby LE <bobby.weiss.le@gmail.com>
2013-04-01 21:02:36 +0200

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fckdabeat::Application.config.secret_token = 'bfc6a56fff4185c588e1e953914d84a2747973488713a889cc83358fded72c66938b5583e96b8c7dcd177b2d4674e755ff4bb699b169c6742f20636a5c5c97f2'
