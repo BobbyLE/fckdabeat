09d232766e6c22634e4b8d87215bd078542d9ea0
Bobby LE <bobby.weiss.le@gmail.com>
2013-04-03 17:05:23 +0200

    add active-record-pk-sequence

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,14 @@ class UsersController < ApplicationController
   before_filter :signed_in_user, only: [:index, :edit, :update]
   before_filter :correct_user,   only: [:edit, :update]
   
+  def show
+    @user = User.find(params[:id])
+  end
+  
+  def edit
+    @user = User.find(params[:id])
+  end
+  
   def create
     @user = User.new(params[:user])
     if @user.save
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Fckdabeat::Application.routes.draw do
+  resources :users
   resources :sessions, only: [:new, :create, :destroy]
   
   match '/signin',  to: 'sessions#new'
