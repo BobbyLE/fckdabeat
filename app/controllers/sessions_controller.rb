cbc4086db14fbedee4eaa6032f6e90607acb309a
Bobby LE <bobby.weiss.le@gmail.com>
2013-04-03 17:39:09 +0200

    show sucess on login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
     user = User.find_by_email(params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       sign_in user
-      flash.now[:success] = "Logged in successful!"
+      flash[:success] = "Logged in successful!"
       redirect_to user
     else
       flash.now[:error] = 'Invalid email/password combination' # Not quite right!
