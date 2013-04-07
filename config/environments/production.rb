de5dc2ad7f633105778db628f5ee573d82d9ee0e
Bobby LE <bobby.weiss.le@gmail.com>
2013-04-07 02:55:17 +0200

    modify production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,12 +67,11 @@ Fckdabeat::Application.configure do
   
     # Amazon S3 settings for Paperclip uploads
   config.paperclip_defaults = {
-    :storage => :s3,
-    :s3_protocol => 'http',
-    :s3_credentials => {
-      :bucket => ENV['AWS_BUCKET'],
-      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+  :storage => :s3,
+  :s3_credentials => {
+    :bucket => ENV['AWS_BUCKET'],
+    :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
     }
   }
 end
