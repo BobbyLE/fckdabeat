edede9dd64d0812057ad9a9e3008d3483e74d408
Bobby LE <bobby.weiss.le@gmail.com>
2013-04-07 18:19:26 +0200

    add category to posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,9 +1,10 @@
 class Post < ActiveRecord::Base
-  attr_accessible :content, :title, :thumbnail
+  attr_accessible :content, :title, :category, :thumbnail
   belongs_to :user
   
   validates :user_id, presence: true
   validates :title, presence: true, length: {maximum: 30}
+  validates :category, presence: true, length: {maximum: 30}
   validates :content, presence: true
   validates_attachment_presence :thumbnail
   
