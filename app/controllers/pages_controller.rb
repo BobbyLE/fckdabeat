9f10857a1fc993f1491256ced26f990cd1ad24a6
Bobby LE <bobby.weiss.le@gmail.com>
2013-04-10 22:45:04 +0200

    modal about us

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,5 +5,7 @@ class PagesController < ApplicationController
     else
       @posts = Post.paginate(:page => params[:page], :per_page => 9)
     end
+    
+    @aboutus = Aboutu.find(1)
   end
 end
