65878eb54d3ede68b0d21caaa64e65a648f98d6d
Bobby LE <bobby.weiss.le@gmail.com>
2013-04-11 22:24:57 +0200

    add public to index

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,9 @@
 class PagesController < ApplicationController
   def index
     if params[:tag]
-      @posts = Post.tagged_with(params[:tag]).paginate(:page => params[:page], :per_page => 9)
+      @posts = Post.where("public = true").tagged_with(params[:tag]).paginate(:page => params[:page], :per_page => 9)
     else
-      @posts = Post.paginate(:page => params[:page], :per_page => 9)
+      @posts = Post.where("public = true").paginate(:page => params[:page], :per_page => 9)
     end
     
     @aboutus = Aboutu.find(1)
